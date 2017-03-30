34528136113fa683295d01e0d947268a59b7ebf6
Krzysztof Pawlowski <Krzysztof@Krzysztofs-MacBook-Pro.local>
2017-03-31 01:06:25 +0200

    refactored to streams

--- Streams/src/main/java/com/example/streams/albums/logic/AlbumsService.java
+++ Streams/src/main/java/com/example/streams/albums/logic/AlbumsService.java
@@ -5,32 +5,15 @@ import com.example.streams.albums.model.Artist;
 import com.example.streams.albums.model.MusicFile;
 import com.example.streams.albums.model.Playlist;
 
-import java.util.List;
-
 public class AlbumsService {
 
-
     public MusicFile getRockFirstMusicFileForArtist(Artist artist) {
-        Album rockAlbum = null;
-        for (Album album : artist.getAlbums()) {
-            if (album.getGenre().equals(Album.Genre.ROCK)) {
-                rockAlbum = album;
-            }
-        }
-
-        if (rockAlbum == null) {
-            return null;
-        }
-
-        Playlist playlist = new Playlist(rockAlbum);
-
-        List<MusicFile> musicFiles = playlist.getMusicFiles();
-
-        if (musicFiles == null || musicFiles.isEmpty()) {
-            return null;
-        }
-
-        return musicFiles.get(0);
+        return artist.getAlbums().stream()
+            .filter(album -> Album.Genre.ROCK.equals(album.getGenre()))
+            .limit(1)
+            .map(Playlist::new)
+            .flatMap(playlist -> playlist.getMusicFiles().stream())
+            .findFirst()
+            .orElse(null);
     }
-
 }
